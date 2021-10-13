6deace7c7d199ff7919eb8e311fe3bb652fe0aa9
unknown <javimorenomontejo@gmail.com>
2021-10-13 14:24:33 +0200

    iteration#1

--- Iteration1.js
+++ Iteration1.js
@@ -1,5 +1,5 @@
 //1.1
-const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
+/*const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
 const div = document.querySelector("div");
 const ul = document.createElement("ul");
 div.appendChild(ul);
@@ -13,10 +13,25 @@ function display(){
         
     }
 }
-display();
+display();*/
 
 //1.2
 const deleted = document.querySelector(".fn-remove-me");
 deleted.remove();
 
 //1.3
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const div = document.querySelector(`div[data-function="printHere"]`);
+const ul = document.createElement("ul");
+div.appendChild(ul);
+
+function display(){
+    for (let i = 0; i < cars.length; i++) {
+        let li = document.createElement("li");
+        let text = document.createTextNode(`${cars[i]}`);
+        li.appendChild(text);
+        ul.appendChild(li);
+    }
+}
+display();
