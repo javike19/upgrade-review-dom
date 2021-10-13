52896f87e9ca87e599df6e8033b92cb234f4b2ac
unknown <javimorenomontejo@gmail.com>
2021-10-13 16:28:07 +0200

    iteration#1

--- Iteration1.js
+++ Iteration1.js
@@ -20,7 +20,7 @@ const deleted = document.querySelector(".fn-remove-me");
 deleted.remove();
 
 //1.3
-const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+/*const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
 
 const div = document.querySelector(`div[data-function="printHere"]`);
 const ul = document.createElement("ul");
@@ -34,4 +34,47 @@ function display(){
         ul.appendChild(li);
     }
 }
-display();
+display();*/
+
+//1.4
+const countries = [
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
+	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
+];
+let body = document.querySelector("body");
+let div = document.createElement("div");
+body.appendChild(div);
+
+function printed(){
+    for (let i = 0; i < countries.length; i++) {
+        let subDiv = document.createElement("div");
+        let para = document.createElement("p");
+        let title = document.createTextNode(`${countries[i].title}`)
+        para.appendChild(title);
+        subDiv.appendChild(para);
+        let img = document.createElement("img");
+        img.src = `${countries[i].imgUrl}`;
+        subDiv.appendChild(img);
+        div.appendChild(subDiv);
+    }
+}
+
+printed();
+
+
+//1.5
+function click(){
+    for (let i = 0; i < countries.length; i++) {
+        if(countries[i] == countries.length){
+            countries.pop;
+            printed();
+        }
+    }
+};
+
+//1.6
+
+//hasta luego...
